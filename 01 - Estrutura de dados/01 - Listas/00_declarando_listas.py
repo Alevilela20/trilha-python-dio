63f094841fd1746bf7b628ed2d88e4f3fc17617d
Alexandre Vilela <alexandrevi2002@gmail.com>
2025-10-03 12:47:46 -0300

    adicionei testes

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -13,5 +13,27 @@ print(numeros)
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
 
+
+
 frutas2 = ["maçã", "laranja", "uva", "pera"]
 print(frutas2[-1])
+
+carros = ["gol", "celta", "palio"]
+for carro in carros:
+    print(carro)
+
+numeros = [1,30,21,2,9,65,34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
+
+
+quadrado = []
+for numero in numeros:
+    quadrado.append(numero ** 2)
+
+print (quadrado)
