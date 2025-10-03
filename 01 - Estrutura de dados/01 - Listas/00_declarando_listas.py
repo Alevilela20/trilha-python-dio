e4c868ffd9b1f349486ce6e49e9a62ad38b77fda
Alexandre Vilela <alexandrevi2002@gmail.com>
2025-10-03 12:28:06 -0300

    frutas 2 adicionado

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -10,5 +10,8 @@ print(letras)
 numeros = list(range(10))
 print(numeros)
 
-carro2 = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
-print(carro2)
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+frutas2 = ["maçã", "laranja", "uva", "pera"]
+print(frutas2[-1])
