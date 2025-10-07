ac986f8234fb069219d5222848d6a5cbd8291ef6
Alexandre Vilela <alexandrevi2002@gmail.com>
2025-10-07 15:10:29 -0300

    testes no declarando tuplas

--- 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -13,3 +13,13 @@ print(numeros)
 
 pais = ("Brasil",)
 print(pais)
+
+frutas2 = ("maça", "laranja", "morango",)
+print(frutas2[1])
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+print(matriz)
